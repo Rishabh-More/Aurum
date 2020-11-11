da14ae7bf410095b7037eb1b160438b3d7571717
Rishabh More <rishabh.more@inviziosolutions.com>
2020-11-11 09:39:51 +0530

    Setting Up App Themes: 1. Installed react-native-paper, react-native-elements 2. Created App Light & Dark Themes in Themes.js 3. Configured Themes in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import React, { createContext, useState } from "react";
+import { Provider as PaperProvider } from "react-native-paper";
+import { AppDefaultTheme, AppDarkTheme } from "./app/res/Themes";
 import { NavigationContainer } from "@react-navigation/native";
 import Authorizer from "./app/navigation/Authorizer";
 
+export const ThemeContext = createContext();
+
 export default function App() {
+  const [isDarkTheme, setDarkTheme] = useState(false);
+  const theme = isDarkTheme ? AppDarkTheme : AppDefaultTheme;
   return (
-    <NavigationContainer>
-      <Authorizer />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <ThemeContext.Provider value={{ isDarkTheme, setDarkTheme }}>
+        <NavigationContainer theme={theme}>
+          <Authorizer />
+        </NavigationContainer>
+      </ThemeContext.Provider>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
---
+++ src/app/res/Themes.js
@@ -0,0 +1,55 @@
+import { DefaultTheme as PaperDefaultTheme, DarkTheme as PaperDarkTheme } from "react-native-paper";
+
+const Colors = {
+  accent: "rgb(255, 152, 0)", //"rgb(192, 24, 91)",
+  accentLight: "rgb(255, 201, 71)", //"rgb(249, 86, 136)",
+  accentDark: "rgb(198, 105, 0)", //"rgb(139, 0, 50)",
+};
+
+const AppDefaultTheme = {
+  name: "Light Theme",
+  dark: false,
+  ...PaperDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    primary: "rgb(255, 255, 255)",
+    accent: Colors.accent,
+    accentDark: Colors.accentDark,
+    accentLight: Colors.accentLight,
+    statusBar: "rgb(232, 232, 232)",
+    background: "rgb(245, 245, 240)",
+    card: "rgb(255, 255, 255)",
+    modal: "rgb(245,245,240)",
+    text: "rgb(28, 28, 30)",
+    textSubtle: "rgb(87, 87, 87)",
+    textInverse: "rgb(255, 255, 255)",
+    border: "rgb(224, 224, 224)",
+    disabled: "rgb(204,204,204)",
+    notification: "rgb(255, 69, 58)",
+  },
+};
+
+const AppDarkTheme = {
+  name: "Dark Theme",
+  dark: true,
+  ...PaperDarkTheme,
+  colors: {
+    ...PaperDarkTheme.colors,
+    primary: "rgb(36, 36, 36)",
+    accent: Colors.accent,
+    accentDark: Colors.accentDark,
+    accentLight: Colors.accentLight,
+    statusBar: "rgb(18, 18, 18)",
+    background: "rgb(18, 18, 18)",
+    card: "rgb(30, 30, 30)",
+    modal: "rgb(55, 55, 55)",
+    text: "rgb(225, 225, 225)",
+    textSubtle: "rgb(245,245,245)",
+    textInverse: "rgb(28, 28, 30)",
+    border: "rgb(87, 87, 87)",
+    disabled: "rgb(96,99,104)",
+    notification: "rgb(255, 69, 58)",
+  },
+};
+
+export { AppDefaultTheme, AppDarkTheme };
