e5450180badaecd3b308a529c4bbc75600bf2ac5
Rishabh More <rishabh.more@inviziosolutions.com>
2020-11-11 08:55:11 +0530

    Installed Native Module Lib: react-native-device-info

--- src/app/screens/Login.js
+++ src/app/screens/Login.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import DeviceInfo from "react-native-device-info";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
-
+//TODO For Now, just use uniqueId for Login Api
 export default function Login() {
+  useEffect(() => {
+    DeviceInfo.getBuildId().then((buildId) => {
+      console.log("Build ID", buildId);
+    });
+    console.log("Device Id", DeviceInfo.getDeviceId());
+    console.log("Unique Id", DeviceInfo.getUniqueId());
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <Text>This is Login Screen</Text>
